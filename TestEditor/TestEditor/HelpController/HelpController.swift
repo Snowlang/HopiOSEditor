30b8e87d1f3a70f4b0b29e9554ac650f6d77f4cd
poisson florent <poissonflorent@MacBook-Pro-de-poisson-2.local>
2018-07-26 16:37:21 +0200

    Add remote language reference access (warning: no language version is handled at this time…)

--- TestEditor/TestEditor/HelpController/HelpController.swift
+++ TestEditor/TestEditor/HelpController/HelpController.swift
@@ -11,31 +11,90 @@ import MarkdownView
 
 class HelpController: UIViewController {
 
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    private let mdView = MarkdownView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        let mdView = MarkdownView()
+        setupMarkdownView()
+        
+        displayLanguageReference()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func setupMarkdownView() {
         mdView.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(mdView)
-    
+        view.sendSubview(toBack: mdView)
+        
         NSLayoutConstraint.activate([
             mdView.topAnchor.constraint(equalTo: view.topAnchor),
             mdView.rightAnchor.constraint(equalTo: view.rightAnchor),
             mdView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             mdView.leftAnchor.constraint(equalTo: view.leftAnchor)
             ])
+    }
     
-        let helpUrl = Bundle.main.url(forResource: "Help", withExtension: "md")!
-        let help = try? String(contentsOf: helpUrl)
+    // MARK: - State management
+    
+    private func getLanguageReference(completion: @escaping (_ languageReference: String?, _ error: Error?) -> Void) {
+
+        let languageReferenceUrl = URL(string: Constants.Editor.languageReferenceUrl)!
         
-        mdView.load(markdown: help)
+        DispatchQueue.global(qos: .utility).async {
+            var languageReference: String!
+            var gettingError: Error!
+            
+            do {
+                languageReference = try String(contentsOf: languageReferenceUrl,
+                                               encoding: String.Encoding.utf8)
+            } catch let error {
+                print("Error: \(error)")
+                gettingError = error
+            }
+            
+            DispatchQueue.main.async {
+                completion(languageReference, gettingError)
+            }
+        }
+    }
+    
+    private func displayLanguageReference() {
+        activityIndicator.startAnimating()
+        
+        getLanguageReference {
+            [weak self] (languageReference, error) in
+            
+            self?.activityIndicator.stopAnimating()
+            
+            if let error = error {
+                self?.displayError(title: NSLocalizedString("Can't get language reference...", comment: ""),
+                                   message: error.localizedDescription)
+            } else {
+                self?.mdView.load(markdown: languageReference)
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func displayError(title: String, message: String) {
+        let alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: .alert)
+        let okAction = UIAlertAction(title: NSLocalizedString("Ok", comment: ""),
+                                     style: .cancel,
+                                     handler: nil)
+        alertController.addAction(okAction)
+        
+        present(alertController,
+                animated: true,
+                completion: nil)
     }
     
     // MARK: - Actions
---
+++ TestEditor/TestEditor/Shared/Commons/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  TestEditor
+//
+//  Created by poisson florent on 26/07/2018.
+//  Copyright © 2018 poisson florent. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+
+    struct Editor {
+        static let languageReferenceUrl = "https://raw.githubusercontent.com/OMTS/Hop/develop/Docs/LanguageReference.md"
+    }
+    
+}
